14a23ed396e22c6381b3b13c6824e9f55946d0d5
Gxarena <gianmarco88arena@gmail.com>
2023-12-14 00:12:30 -0500

    Added Weather App

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WorkIMG from "../assets/apexAPP.png";
+import WeatherApp from '../assets/WeatherApp.png'
 
 const Projects = () => {
   return (
@@ -43,22 +44,22 @@ const Projects = () => {
           </div>
 
           <div
-            style={{ backgroundImage: `url(${WorkIMG})` }}
+            style={{ backgroundImage: `url(${WeatherApp})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className=" text-2xl font-bold text-white tracking-wider flex justify-center text-center">
-                Apex Legends Player Stat Tracker
+              <span className=" text-2xl font-bold text-white tracking-wider drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] flex justify-center text-center">
+                Weather App
               </span>
               <div className="pt-8 text-center">
-                <a href="https://apexlegendstracker.netlify.app/">
+                <a href="https://weatheapponline.netlify.app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-black hover:bg-opacity-80 hover:text-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a
                   target="_blank"
-                  href="https://github.com/Gxarena/Apex-Legends-Tracker"
+                  href="https://github.com/Gxarena/WeatherAPI"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-black hover:bg-opacity-80 hover:text-white text-gray-700 font-bold text-lg">
                     Code
